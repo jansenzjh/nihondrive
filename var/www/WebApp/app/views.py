a24112e5c9578f6de50db5a8dc77320657c4fd73
Jansen <jansenzjh@gmail.com>
2022-10-08 15:35:27 +0900

    added honmen support

--- var/www/WebApp/app/views.py
+++ var/www/WebApp/app/views.py
@@ -1,3 +1,4 @@
+from json import load
 from app import app
 from flask import render_template
 import csv
@@ -13,10 +14,13 @@ class Ques:
         self.Question = var1
         self.Answer = var2
 
+def get_bool(s):
+    return s.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']
+
 
 @app.route('/')
 def home():
-    return "<b>There has been a change!!!!!!@@@@@</b>"
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
@@ -31,8 +35,8 @@ def hello():
 def template():
     return render_template('home.html')
 
-@app.route('/karimen')
-def karimen():
+@app.route('/karimen/<load_all>', methods=['GET', 'POST'])
+def karimen(load_all):
     questions = []
 
     try:
@@ -45,4 +49,26 @@ def karimen():
     except Exception as error:
         return str(error)
 
-    return render_template('karimen.html', len=len(questions[:50]), questions=questions)
+    if get_bool(load_all):
+        return render_template('question_view.html', question_type="karimen all", len=len(questions), questions=questions)
+    else:
+        return render_template('question_view.html', question_type="karimen", len=len(questions[:50]), questions=questions)
+
+@app.route('/honmen/<load_all>', methods=['GET', 'POST'])
+def honmen(load_all):
+    questions = []
+
+    try:
+        with open('app/Honmen.csv', 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                questions.append(Ques(row[1], row[4]))
+
+            random.shuffle(questions)
+    except Exception as error:
+        return str(error)
+
+    if get_bool(load_all):
+        return render_template('question_view.html', question_type="honmen all", len=len(questions), questions=questions)
+    else:
+        return render_template('question_view.html', question_type="honmen", len=len(questions[:100]), questions=questions)
